dc2b368afab1afaa286b1a88b2b7faac7e5fc6f2
Louis Bohorquez <louis.bohorquez@roadster.com>
2019-06-04 16:34:37 +0000

    [Mode] Updated to include Key Contacts from SF.

--- Mode/roadster/spaces/Team SALES OPS/Agent Contact Data.c6373e92fd3f/Agent Contact Data.00fe65a7cf81.sql
+++ Mode/roadster/spaces/Team SALES OPS/Agent Contact Data.c6373e92fd3f/Agent Contact Data.00fe65a7cf81.sql
@@ -9,11 +9,13 @@ WITH SF_Account as (
                     ,dp.id as dpid_id
                     ,dp.primary_make
                     ,acc."Sales_Type__c"  sales_type
+                    ,sf.sf_account_id as sf_account_id
             FROM fact.salesforce_dealer_info  sf
             LEFT JOIN dealer_partners dp on sf.dpid=dp.dpid
             LEFT JOIN roadster_salesforce."Account" acc on sf.sf_account_id=acc."Id"
+
             WHERE date(sf.actual_live_date) >= '2019-04-28'
-            and acc."NPS_Survey_Sent_On__c" is null
+            and acc."NPS_Survey_Sent_On__c" is  null
             
             ),
             
@@ -26,7 +28,37 @@ agent_detail as (
             INNER JOIN SF_Account sf on a.dealer_partner_id=sf.dpid_id
             WHERE a.status='Active'
             AND email not like '%roadster%'
-            )        
+            ),
+key_contact_detail as  (
+            SELECT "FirstName" as first_name
+                  ,"LastName" as last_name
+                  ,"Email" as email
+                  ,ACR."AccountId" as sf_account_id
+                  ,ACR."Roles" as roles
+            FROM  roadster_salesforce."AccountContactRelation" ACR
+            left join roadster_salesforce."Contact" C on ACR."ContactId"=c."Id"
+            INNER JOIN SF_account sf on ACR."AccountId"=sf.sf_account_id
+            where "Roles"<>'Billing Contact'      
+              )          
+
+SELECT "Actual Live Date"
+        ,"Account Name"
+        ,"Dealer Group"
+        ,"Billing State/Province"
+        ,a.first_name as "First Name"
+        ,a.last_name as "Last Name"
+        ,a.email as "Email"
+        ,roles as "Roles"
+        ,"Account Owner: Full Name"
+        ,"Success Owner: Full Name"
+        ,"Integration Manager"
+        ,sales_type as "Sales Type"
+        ,primary_make as "OEM Name"
+FROM key_contact_detail a
+INNER JOIN SF_Account sf on a.sf_account_id=sf.sf_account_id
+where email is null
+
+UNION
             
 SELECT "Actual Live Date"
         ,"Account Name"
