ac9637830bb7cdd49f6376c0823877e50451d2e2
George Jacobs <george.jacobs@roadster.com>
2019-06-11 23:31:34 +0000

    [Mode] Update Report 'Agent Report - Alpha'

--- Mode/roadster/spaces/_DEV SANDBOX/Agent Report - Alpha.4ad9f32036c7/Agent Monthly Data.e4cc32ac6607.sql
+++ Mode/roadster/spaces/_DEV SANDBOX/Agent Report - Alpha.4ad9f32036c7/Agent Monthly Data.e4cc32ac6607.sql
@@ -1,4 +1,8 @@
 -- Generates the data
-SELECT *
-FROM fact.agg_agent_report_monthly
+SELECT am.*,
+       CASE 
+       WHEN "Recent Activity Timestamp" IS NULL then 'x'
+       WHEN extract('day' from (now() - "Recent Activity Timestamp")) > 7 THEN 'x' 
+       else '✓' END as check_for_activity
+FROM report_layer.agent_report am
 WHERE dpid = '{{ dpid }}'
