add2ad0ef1240f16ca92eed6b926f62951ca3dac
George Jacobs <george.jacobs@roadster.com>
2019-06-28 22:13:38 +0000

    [Mode] Update Report 'Agent Report - Alpha'

--- Mode/roadster/spaces/~PRODUCTION (LOCKED)/Agent Report - Alpha.4ad9f32036c7/Agent Monthly Data.e4cc32ac6607.sql
+++ Mode/roadster/spaces/~PRODUCTION (LOCKED)/Agent Report - Alpha.4ad9f32036c7/Agent Monthly Data.e4cc32ac6607.sql
@@ -1,8 +1,15 @@
 -- Generates the data
-SELECT am.*,
+with data as (SELECT am.*,
        CASE 
        WHEN "Recent Activity Timestamp" IS NULL then 'x'
        WHEN extract('day' from (now() - "Recent Activity Timestamp")) > 7 THEN 'x' 
        else '✓' END as check_for_activity
 FROM report_layer.agent_report am
 WHERE dpid = '{{ dpid }}'
+AND "Start Date of Metrics" = (SELECT max("Start Date of Metrics") FROM report_layer.agent_report)
+)
+
+SELECT ROW_NUMBER() OVER (ORDER BY dpid) Id, 
+data.*,
+REPLACE("Certification Completed", ',', '<br>') "Completed New"
+FROM data
