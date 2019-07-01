0ea61e9e62d097afe8e5ce1af271318d3c84d850
George Jacobs <george.jacobs@roadster.com>
2019-07-01 18:12:03 +0000

    [Mode] Update Report 'Agent Report - Alpha'

--- Mode/roadster/spaces/~PRODUCTION (LOCKED)/Agent Report - Alpha.4ad9f32036c7/Agent Monthly Data.e4cc32ac6607.sql
+++ Mode/roadster/spaces/~PRODUCTION (LOCKED)/Agent Report - Alpha.4ad9f32036c7/Agent Monthly Data.e4cc32ac6607.sql
@@ -6,7 +6,7 @@ with data as (SELECT am.*,
        else '✓' END as check_for_activity
 FROM report_layer.agent_report am
 WHERE dpid = '{{ dpid }}'
-AND "Start Date of Metrics" = (SELECT max("Start Date of Metrics") FROM report_layer.agent_report)
+AND "Start Date of Metrics" > (date_trunc('month', now()) - '2 mons'::interval)
 )
 
 SELECT ROW_NUMBER() OVER (ORDER BY dpid) Id, 
