92ee38e132879aeb686e7ca491c16bc6a02b6a28
Sean Kervin <sean.kervin@roadster.com>
2019-07-11 17:26:00 +0000

    [Mode] Rename report 'Dealer Properties [All DPIDs] (Current)'

--- Mode/roadster/spaces/Team ANALYTICS/Dealer Properties [All DPIDs].ab6fbbd64d31/Dealer Partner Properties.70d43363f5ba.sql
+++ Mode/roadster/spaces/Team ANALYTICS/Dealer Properties [All DPIDs].ab6fbbd64d31/Dealer Partner Properties.70d43363f5ba.sql
@@ -1,7 +1,5 @@
 
 
-
-
 with traffic as ( select *
 from fact.mode_agg_daily_traffic_and_prospects ft
 where ft.date between current_date-8 and current_date-1
@@ -56,7 +54,7 @@ SELECT
        case when admin.properties ->> 'embedded_checkout_frame' = 'true' then 'Slide Out' else Null end as "Slide-out",
        admin.properties ->> 'cdk_extract_id' "CDK Sales Match",
        to_char(sf.actual_live_date, 'yyyy-mm-dd') "Go Live Date",
-       dealer_visitors.count "Dealer Visitors past wk",
+       case when dealer_visitors.count is null then 'Script not Installed' else dealer_visitors.count::text end as "Dealer Visitors past wk",
        online_express_traffic.count "Online ES Visitors past wk",
        instoreP."In Store" + instoreP."Online" "Prospects",
        instoreO."In Store" + instoreO."Online" "Orders",
@@ -75,6 +73,22 @@ SELECT
        dp.timezone,
        sf.status "SF Status",
        dp.status "Dealer.Admin Status"
+       
+       ------ KBB ICO TRADE FLAGS ------
+       --admin.properties ->> 'kbb_ico_iframe_url' "kbb_ico_iframe_url",
+       --admin.properties ->> 'kbb_widget_api_key' "kbb_widget_api_key",
+       --admin.properties ->> 'fetch_kbb_tradein_value' "fetch_kbb_tradein_value",
+       --admin.properties ->> 'kbb_valuation_supported' "kbb_valuation_supported",
+       --admin.properties ->> 'external_trade_valuation' "external_trade_valuation",
+       --admin.properties ->> 'tradein_url' "tradein_url",
+       --admin.properties ->> 'allow_standalone_trade' "allow_standalone_trade",
+       --admin.properties ->> 'instant_trade_valuation' "instant_trade_valuation",
+       --admin.properties ->> 'ico_params' "ico_params",
+       --admin.properties ->> 'custom_ico_url' "custom_ico_url"
+       
+       
+       
+       
        --,admin.properties 
 
 FROM dealer_partner_properties admin
@@ -86,8 +100,17 @@ LEFT JOIN online_express_traffic on online_express_traffic.dpid = dp.dpid
 LEFT JOIN instoreP on instoreP.dpid = dp.dpid
 LEFT JOIN instoreO on instoreO.dpid = dp.dpid
 
-WHERE admin.date = date_trunc('day',now()-interval'1 day')::date
+WHERE admin.date = '{{admin_date}}'::date
 and (sf.status = 'Live' or dp.status = 'Live')
 
 ORDER BY dp.dpid;
 
+
+{% form %}
+
+admin_date:
+  type: date
+  default: {{ 'now' | date: '%s' | minus: 111600 | date: '%Y-%m-%d' }}
+  description: Show what Dealer Admin looked like on this date
+
+{% endform %}
