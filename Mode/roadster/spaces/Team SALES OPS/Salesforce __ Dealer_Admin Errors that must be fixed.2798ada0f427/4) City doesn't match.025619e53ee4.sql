af2e7374869a0a18d6518a5306775252804a84cd
Sean Kervin <sean.kervin@roadster.com>
2019-07-26 17:38:57 +0000

    [Mode] removed marketplaces from 'cities don't match'

--- Mode/roadster/spaces/Team SALES OPS/Salesforce __ Dealer_Admin Errors that must be fixed.2798ada0f427/4) City doesn't match.025619e53ee4.sql
+++ Mode/roadster/spaces/Team SALES OPS/Salesforce __ Dealer_Admin Errors that must be fixed.2798ada0f427/4) City doesn't match.025619e53ee4.sql
@@ -1,16 +1,29 @@
--- Returns first 100 rows from public.dealer_partners
+
+
+
+
+
+
+
 SELECT sf.integration_manager "Integration Manager",
+      admin.dpid as "Admin DPID", 
       admin.City as "Admin City", 
       sf.City as "Salesforce City", 
-      admin.dpid as "Admin DPID", 
+      sf.address as "SF Address",
+      admin.address "Admin Address",
       admin.name as "Admin Dealer Name",
-      sf.status as "Salesforce Status"
+      sf.status as "Salesforce Status",
+      sfi."Base_Product_Type__c" as "Base Product"
+      
 
 FROM public.dealer_partners admin
 
 left join fact.salesforce_dealer_info sf on admin.dpid = sf.dpid
+left join roadster_salesforce."Integration__c" sfi on admin.dpid = sfi."DealerPartnerID__c"
 
 where admin.city <> sf.city
 and sf.status <> 'Cancelled'
+and sf.status <> 'Pending Cancellation'
+and sfi."Base_Product_Type__c" <> 'Marketplace'
 
 order by sf.integration_manager, admin.dpid 
