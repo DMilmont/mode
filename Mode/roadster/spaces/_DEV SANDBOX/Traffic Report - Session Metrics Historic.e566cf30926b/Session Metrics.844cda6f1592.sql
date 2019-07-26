77886c59326ad21af5c6e73611531e5736b219a0
Louis Bohorquez <louis.bohorquez@roadster.com>
2019-07-26 02:59:32 +0000

    [Mode] Rename report 'Traffic Report - Session Historic'

--- Mode/roadster/spaces/_DEV SANDBOX/Traffic Report - Session Metrics Historic.e566cf30926b/Session Metrics.844cda6f1592.sql
+++ Mode/roadster/spaces/_DEV SANDBOX/Traffic Report - Session Metrics Historic.e566cf30926b/Session Metrics.844cda6f1592.sql
@@ -1,13 +1,13 @@
 select *
 
 from report_layer.ga2_session_metrics
-where type = '{{ category }}' 
+where type = '{{ segment }}' 
 AND dpid='{{ dpid }}'
 and session_type <> 'Dealer Admin'
 {% form %}
 
 
-category:
+segment:
     type: select
     default: Main Site or Express Site
     options: [Main Site or Express Site, Express New or Used Shoppers,Express SRP or VDP Viewers,Acquisition Type]
