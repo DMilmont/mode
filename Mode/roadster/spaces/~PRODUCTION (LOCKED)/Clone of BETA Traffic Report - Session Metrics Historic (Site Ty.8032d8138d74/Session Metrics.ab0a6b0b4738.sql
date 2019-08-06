161052f4bd9c6665089d75f2e8249e912befa6f9
Louis Bohorquez <louis.bohorquez@roadster.com>
2019-08-06 23:26:48 +0000

    [Mode] Create report 'Clone of BETA Traffic Report - Session Metrics Historic (Site Ty'

---
+++ Mode/roadster/spaces/~PRODUCTION (LOCKED)/Clone of BETA Traffic Report - Session Metrics Historic (Site Ty.8032d8138d74/Session Metrics.ab0a6b0b4738.sql
@@ -0,0 +1,15 @@
+select *
+
+from report_layer.ga2_session_metrics
+where type = 'Express New or Used Shoppers' 
+AND dpid='{{ dpid }}'
+and session_type <> 'Dealer Admin'
+
+
+--{% form %}
+
+-- segment:
+--     type: select
+--     default: Main Site or Express Site
+--     options: [Main Site or Express Site, Express New or Used Shoppers,Express SRP or VDP Viewers,Acquisition Type]
+-- {% endform %}
