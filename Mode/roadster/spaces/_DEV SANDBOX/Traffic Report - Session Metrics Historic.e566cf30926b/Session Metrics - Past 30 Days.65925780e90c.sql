37873331086f29327b68a048e374713fdf980a4e
Louis Bohorquez <louis.bohorquez@roadster.com>
2019-09-09 22:27:05 +0000

    [Mode] Added Javascript for Bullet Charts

--- Mode/roadster/spaces/_DEV SANDBOX/Traffic Report - Session Metrics Historic.e566cf30926b/Session Metrics - Past 30 Days.65925780e90c.sql
+++ Mode/roadster/spaces/_DEV SANDBOX/Traffic Report - Session Metrics Historic.e566cf30926b/Session Metrics - Past 30 Days.65925780e90c.sql
@@ -12,7 +12,7 @@ select month_year
       ,case when left(avg_session_duration_time,1)='0' then substr(avg_session_duration_time,2) else avg_session_duration_time end as avg_session_duration_time
       ,avg_session_duration_seconds
       ,pageviews
-      ,pageviews_per_session
+      ,round(pageviews_per_session,1) as pageviews_per_session
       ,round(bounce_rate*100) || '%' as bounce_rate_perc
 from report_layer.session_metrics_past30days
 where  dpid='{{ dpid }}'
