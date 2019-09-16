4a630f42f726de56c00b7c7fec8b3bb12f877847
Louis Bohorquez <louis.bohorquez@roadster.com>
2019-09-16 18:11:41 +0000

    [Mode] Create report 'Demo Top Referrer and Landing Pages V2- BETA'

---
+++ Mode/roadster/spaces/Demo Dealer Reports/Demo Top Referrer and Landing Pages V2- BETA.41b8a278e82d/Express Landing Pages.7aab255adb7b.sql
@@ -0,0 +1,1 @@
+select * from fact.zdemo_Traffic_Landing_Pages
---
+++ Mode/roadster/spaces/Demo Dealer Reports/Demo Top Referrer and Landing Pages V2- BETA.41b8a278e82d/select_box.000fdf98087b.sql
@@ -0,0 +1,15 @@
+with tab1 as (select distinct ((to_char(a.month_year, 'Month'::text) || ''::text) ||
+          to_char(a.month_year, 'YYYY'::text))                                                          AS "Month & Year"
+         ,date_part('month'::text, a.month_year)                                                       AS "Data Month"
+        , date_part('year'::text, a.month_year)                                                        AS "Data Year"
+
+
+FROM fact.zdemo_traffic_landing_pages a
+WHERE  a.month_year >= (date_trunc('day', now()) - INTERVAL '13 Months')
+and a.month_year <= (date_trunc('day', now()) - INTERVAL '1 Months')
+ORDER BY "Data Year", "Data Month"
+)
+
+SELECT 
+"Month & Year"
+FROM tab1
