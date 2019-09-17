2babd8a4901ea6b10da32ff4d4f1835126fd9ddd
Sean Kervin <sean.kervin@roadster.com>
2019-09-17 15:15:56 +0000

    [Mode] Rename report 'Matched Sales Audit _CDK_ [One DPID] (All History)'

--- Mode/roadster/spaces/Team ANALYTICS/Matched Sales Audit _CDK_ [One DPID] (All History).7934d29af550/Dealers with both CDK and DS.e8ccf3756fa8.sql
+++ Mode/roadster/spaces/Team ANALYTICS/Matched Sales Audit _CDK_ [One DPID] (All History).7934d29af550/Dealers with both CDK and DS.e8ccf3756fa8.sql
@@ -2,12 +2,13 @@ with tab1 as (
 SELECT 
     dp.name "Dealer Name",
     dp.dpid,
-    crm.dealer_partner_id,
+    sf.dealer_group,
+    --crm.dealer_partner_id,
     --to_char(crm.sold_at,'YYYY-Mon') "Sold Month",
     --item_type,
     --crm_type,
-    sum(case when crm_type = 'dealer-socket' then 1 else null end) "Dealer Socket sales",
-    sum(case when crm_type = 'cdk' then 1 else null end) "CDK sales"
+    sum(case when crm_type = 'dealer-socket' then 1 else null end) "Dealer Socket sales past 3mo",
+    sum(case when crm_type = 'cdk' then 1 else null end) "CDK sales past 3mo"
    -- min(case when crm_type = 'cdk' then crm.sold_at else null end) as "First CRM Sales Record",
     -- min(case when crm_type = 'dealer-socket' then crm.sold_at else null end) as "First DS Sales Record"
     
@@ -19,11 +20,12 @@ left join fact.salesforce_dealer_info sf on sf.dpid = dp.dpid
 left join fact.f_sale s on  s.crm_record_id = crm.id 
 
 where crm.status = 'Sold'
-  and crm.sold_at > date '01-01-2019'
+  and crm.sold_at > now() - interval '3 months' 
   
 group by 1,2,3
 
 order by dp.name asc)
 
 select * from tab1
-where "Dealer Socket sales" > 0 and "CDK sales" >0 
+where "Dealer Socket sales past 3mo" > 0 and "CDK sales past 3mo" >0 
+order by dealer_group asc, dpid asc 
