d3bd20ee2cb4392c52a4dae517e4137780ccd977
George Jacobs <george.jacobs@roadster.com>
2019-10-10 16:49:49 +0000

    [Mode] Update Report 'Agent Report'

---
+++ Mode/roadster/spaces/~PRODUCTION (LOCKED)/Agent Report.4ad9f32036c7/select_box.1c8a736e0170.sql
@@ -0,0 +1,9 @@
+WITH tab1 as (
+SELECT DISTINCT "Data Year", "Data Month", "Month & Year"
+FROM report_layer.agent_report_monthly_table am
+ORDER BY "Data Year", "Data Month"
+)
+
+SELECT 
+"Month & Year"
+FROM tab1
