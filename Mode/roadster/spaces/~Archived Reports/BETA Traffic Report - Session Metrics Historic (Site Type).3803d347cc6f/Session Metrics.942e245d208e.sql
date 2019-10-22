ef70113143c8d105aa9bd5d778a431cc1838398e
Louis Bohorquez <louis.bohorquez@roadster.com>
2019-10-22 16:15:38 +0000

    [Mode] Move report 'BETA Traffic Report - Session Metrics Historic (Site Type)'

--- Mode/roadster/spaces/~Archived Reports/BETA Traffic Report - Session Metrics Historic (Site Type).3803d347cc6f/Session Metrics.942e245d208e.sql
+++ Mode/roadster/spaces/~Archived Reports/BETA Traffic Report - Session Metrics Historic (Site Type).3803d347cc6f/Session Metrics.942e245d208e.sql
@@ -1,6 +1,6 @@
 select *
 
-from report_layer.ga2_session_metrics
+from fact.agg_monthly_session_metrics
 where type = 'Main Site or Express Site' 
 AND dpid='{{ dpid }}'
 and session_type <> 'Dealer Admin'
